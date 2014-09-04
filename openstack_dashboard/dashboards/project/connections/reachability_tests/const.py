e2f949e3a1122195f34ae7c410c1bd7fcf0c06f8
xinwu <xin.wu@gmail.com>
2014-09-04 07:55:29 -0700

    add missing files

---
+++ openstack_dashboard/dashboards/project/connections/reachability_tests/const.py
@@ -0,0 +1,26 @@
+from neutron.db import models_v2
+from neutron.db import model_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from openstack_dashboard.dashboards.project.connections.reachability_tests.reachability_test_db \
+      import ReachabilityTest, ReachabilityTestResult, ReachabilityQuickTest, ReachabilityQuickTestResult
+
+controller = "172.16.54.234"
+port = 8080
+username = 'admin'
+password = 'adminadmin'
+
+db_ip = '127.0.0.1'
+db_user = 'root'
+db_pwd = 'password'
+tenant_id = 'admin'
+engine_string = "mysql+mysqldb://%s:%s@%s/neutron?charset=utf8" % (db_user, db_pwd, db_ip)
+engine = create_engine(engine_string)
+Session = sessionmaker(bind=engine, expire_on_commit=False)
+Base = model_base.BASEV2()
+Base.metadata.create_all(bind=engine)
+
+def debug(msg):
+    f = open('haha', 'a')
+    f.write(msg)
+    f.close()
