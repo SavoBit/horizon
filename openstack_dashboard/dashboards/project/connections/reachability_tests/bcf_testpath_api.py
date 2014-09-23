c97d88c78f19ed7e099e16ee6ac46ce140d64956
Kevin Benton <blak111@gmail.com>
2014-09-22 20:18:36 -0700

    Fix for active/standby detection

--- openstack_dashboard/dashboards/project/connections/reachability_tests/bcf_testpath_api.py
+++ openstack_dashboard/dashboards/project/connections/reachability_tests/bcf_testpath_api.py
@@ -13,6 +13,7 @@ URL_TEST_PATH = ('applications/bcf/test/path/controller-view'
                  '[dst-tenant=\"%(dst-tenant)s\"]'
                  '[dst-segment=\"%(dst-segment)s\"][dst-ip=\"%(dst-ip)s\"]')
 
+
 class ControllerCluster(object):
     """
     Controller interfaces for a big switch controller cluster
@@ -25,25 +26,58 @@ class ControllerCluster(object):
 
     @property
     def active_controller(self):
-        # return first-responder
         pool = eventlet.greenpool.GreenPool()
         coroutines = {}
+        active = None
+        standby_offline = []
         for controller in self.controllers:
-            coroutines[controller] = pool.spawn(
-                rest_lib.request, "", host="%s:%d" % (controller, self.port))
-        while True:
-            for controller in coroutines:
+            coroutines[controller] = pool.spawn(self.auth, controller)
+        while coroutines:
+            completed = None
+            for controller in coroutines.keys():
                 if coroutines[controller].dead:
-                    return controller
+                    completed = controller
+                    break
             eventlet.sleep(0.1)
-        # neither responded in time, return first in list
+            if completed:
+                coro = coroutines.pop(completed)
+                try:
+                    cookie = coro.wait()
+                    url = 'core/controller/role'
+                    res = rest_lib.get(cookie, url, controller,
+                                       self.port)[2]
+                    if 'active' in res:
+                        active = controller
+                        self.cookie = cookie
+                    else:
+                        standby_offline.append((cookie, controller))
+                        self.logout(cookie, controller)
+                except:
+                    standby_offline.append(('', controller))
+            if active:
+                pool.spawn(self.cleanup_remaining, standby_offline, coroutines)
+                return active
+        # none responded in time, return first in list
         return self.controllers[0]
 
-    def auth(self, username=bsn_api.username, password=bsn_api.password):
+    def cleanup_remaining(self, finished, waiting):
+        for cookie, controller in finished:
+            if not cookie:
+                continue
+            self.logout(cookie, controller)
+        for controller in waiting.keys():
+            try:
+                cookie = waiting[controller].wait()
+                self.logout(cookie, controller)
+            except:
+                pass
+
+    def auth(self, server, username=bsn_api.username,
+             password=bsn_api.password):
         login = {"user": username, "password": password}
-        host = "%s:%d" % (self.active_controller, self.port)
+        host = "%s:%d" % (server, self.port)
         log.info("PRE AUTH: Host: %s\t user: %s password: %s"
-                 % (self.active_controller, username, password))
+                 % (server, username, password))
         ret = rest_lib.request("/api/v1/auth/login", prefix='',
                                method='POST', data=json.dumps(login),
                                host=host)
@@ -53,25 +87,24 @@ class ControllerCluster(object):
         if ("session_cookie" not in session):
             raise Exception("Failed to authenticate: session cookie not set")
 
-        self.cookie = session["session_cookie"]
-        return self.cookie
+        return session["session_cookie"]
 
-    def logout(self):
-        url = "core/aaa/session[auth-token=\"%s\"]" % self.cookie
-        ret = rest_lib.delete(self.cookie, url, self.active_controller,
-                              self.port)
-        log.info("LOGOUT Session: cookie: %s" % self.cookie)
+    def logout(self, cookie, controller):
+        url = "core/aaa/session[auth-token=\"%s\"]" % cookie
+        ret = rest_lib.delete(cookie, url, controller, self.port)
+        log.info("LOGOUT Session: cookie: %s" % cookie)
         return ret
 
     def getTestPath(self, src, dst):
         url = (URL_TEST_PATH %
-              {'src-tenant': src['tenant'],
-               'src-segment': src['segment'],
-               'src-ip': src['ip'],
-               'dst-tenant': dst['tenant'],
-               'dst-segment': dst['segment'], 'dst-ip': dst['ip']})
+               {'src-tenant': src['tenant'],
+                'src-segment': src['segment'],
+                'src-ip': src['ip'],
+                'dst-tenant': dst['tenant'],
+                'dst-segment': dst['segment'], 'dst-ip': dst['ip']})
+        controller = self.active_controller
         ret = rest_lib.get(self.cookie, url,
-                           self.active_controller, self.port)
+                           controller, self.port)
         data = list()
         if ret[0] in range(200, 300):
             data = json.loads(ret[2])
